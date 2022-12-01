fac0d0c1261606754bbb68d58d8cad60e4436815
Serpener <vladserpener@gmail.com>
2022-12-02 00:03:34 +0300

    AddProject

---
+++ IJunior/AquariumFish/AquariumWithFish.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AquariumWithFish
+{
+    internal class AquariumWithFish
+    {
+
+        static void Main(string[] args)
+        {
+            Aquarium aquarium = new Aquarium(10);
+            MenuAquarium menuAquarium = new MenuAquarium(aquarium);
+        }
+    }
+
+    class MenuAquarium
+    {
+        private const string CommandAddFish = "AddFish";
+        private const string CommandRemoveFish = "RemoveFish";
+        private const string CommandExit = "Exit";
+        private Aquarium _aquarium;
+        private bool _isWork = true;
+
+        public MenuAquarium(Aquarium aquarium)
+        {
+            _aquarium = aquarium;
+
+            while (_isWork)
+            {
+                Console.Clear();
+
+                aquarium.ShowInfoAllFish();
+                PrintMenuItems();
+                StartMenuLogic();
+                aquarium.SkipOneMonthFishLife();
+            }
+        }
+
+        private void PrintMenuItems()
+        {
+            Console.WriteLine($"[{CommandAddFish}]\t- Добавить рыбку в аквариум");
+            Console.WriteLine($"[{CommandRemoveFish}]\t- Убрать рыбку из аквариума");
+            Console.WriteLine($"[{CommandExit}]\t\t- Выйти из приложения");
+        }
+
+        private void StartMenuLogic()
+        {
+            Console.Write("Выберите пункт меню: ");
+            string userInputMeniItem = Console.ReadLine();
+
+            switch (userInputMeniItem)
+            {
+                case CommandAddFish:
+                    AddFish();
+                    break;
+
+                case CommandRemoveFish:
+                    RemoveFish();
+                    break;
+
+                case CommandExit:
+                    _isWork = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Вы выбрали не существующествующий пунк в меню!");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+
+        private void AddFish()
+        {
+            if (_aquarium.MaxCountFish > _aquarium.GetCountFish())
+            {
+                Console.Clear();
+                _aquarium.ShowInfoAllFish();
+
+                Console.Write("Введите имя рыбы: ");
+                string userInputName = Console.ReadLine();
+
+                Console.Write("Введите количесвто лет которое может прожить эта рыба: ");
+                bool isConverted = int.TryParse(Console.ReadLine(), out int userInputMaxAge);
+
+                if (isConverted)
+                {
+                    Fish tempFfish = new Fish(userInputName, userInputMaxAge);
+                    _aquarium.AddFish(tempFfish);
+                }
+                else
+                {
+                    PrintMessageMistake();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Аквариум заполнен!");
+            }
+
+            Console.ReadKey();
+        }
+
+        private void RemoveFish()
+        {
+            if (_aquarium.GetCountFish() > 0)
+            {
+                Console.Clear();
+                _aquarium.ShowInfoAllFish();
+                bool isConverted;
+
+                Console.Write("Выберите ID рыбы: ");
+                isConverted = int.TryParse(Console.ReadLine(), out int numberFish);
+
+                if (isConverted)
+                {
+                    if (_aquarium.GetCountFish() >= numberFish)
+                    {
+                        _aquarium.RemoveFish(numberFish - 1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы выбрали не существующую рыбу!");
+                    }
+                }
+                else
+                {
+                    PrintMessageMistake();
+                }
+            }
+            else
+            {
+                Console.WriteLine("В аквариуме нет рыб!");
+            }
+
+            Console.ReadKey();
+        }
+
+        private void PrintMessageMistake()
+        {
+            Console.WriteLine("Ошибка!");
+        }
+    }
+
+    class Aquarium
+    {
+        private List<Fish> _fish = new List<Fish>();
+
+        public int MaxCountFish { get; private set; }
+
+        public Aquarium(int maxCountFish)
+        {
+            MaxCountFish = maxCountFish;
+        }
+
+        public void AddFish(Fish fish)
+        {
+            _fish.Add(fish);
+        }
+
+        public void RemoveFish(int id)
+        {
+            _fish.Remove(_fish[id]);
+        }
+
+        public int GetCountFish()
+        {
+            return _fish.Count;
+        }
+
+        virtual public void ShowInfoAllFish()
+        {
+            int CursorPisitionLeft = 70;
+
+            Console.SetCursorPosition(CursorPisitionLeft, 0);
+            Console.WriteLine($"Рыб в аквариуме {_fish.Count}/{MaxCountFish}");
+
+            for (int i = 0; i < _fish.Count; i++)
+            {
+                Console.SetCursorPosition(CursorPisitionLeft, i + 1);
+                _fish[i].ShowInfo();
+            }
+
+            Console.SetCursorPosition(0, 0);
+        }
+
+        public void SkipOneMonthFishLife()
+        {
+            foreach (var fish in _fish)
+            {
+                fish.MissingMonth();
+            }
+        }
+    }
+
+    class Fish
+    {
+        private static int _ids;
+        private bool IsAlive = true;
+        private int _countMonthOneYear = 12;
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public int MaxAgeInMonth { get; private set; }
+
+        public int Number { get; private set; }
+
+        public Fish(string name, int maxAge)
+        {
+            Number = ++_ids;
+            IsAlive = true;
+            Age = 0;
+            Name = name;
+            MaxAgeInMonth = maxAge * _countMonthOneYear;
+        }
+
+        public void ShowInfo()
+        {
+            if (IsAlive)
+            {
+                Console.Write("#{0}\t|Age: {1}/{2}.мес\t| Name: {3} ", Number, Age, MaxAgeInMonth, Name);
+            }
+            else
+            {
+                Console.Write("#{0}\t|Рыбка померла, лучше её убрать!|", Number);
+            }
+        }
+
+        public void MissingMonth()
+        {
+            Age++;
+
+            if (Age > MaxAgeInMonth)
+            {
+                IsAlive = false;
+            }
+        }
+    }
+}
---
+++ IJunior/War/War.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace War
+{
+    internal class War
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.SetWindowSize(107, 45);
+            Console.SetBufferSize(107, 45);
+            Console.Title = "War, war never changes!";
+
+            Battlefield battlefield = new Battlefield();
+            battlefield.StartWar();
+            battlefield.ShowResultWar();
+        }
+    }
+
+    class Battlefield
+    {
+        private Platoon _firstPlatoon;
+        private Platoon _secondPlatoon;
+
+        public Battlefield()
+        {
+            int _leftIndentFirstCountry = 0;
+            int _leftIndentSecondCountry = 55;
+            _firstPlatoon = new Platoon(_leftIndentFirstCountry);
+            _secondPlatoon = new Platoon(_leftIndentSecondCountry);
+        }
+
+        public void StartWar()
+        {
+            _firstPlatoon.ShowInfoForAllWarriors();
+            _secondPlatoon.ShowInfoForAllWarriors();
+
+            while (_firstPlatoon.GetCount() > 0 && _secondPlatoon.GetCount() > 0)
+            {
+                Console.ReadKey();
+                Console.Clear();
+
+                Attack(_firstPlatoon, _secondPlatoon);
+                Attack(_secondPlatoon, _firstPlatoon);
+
+                _firstPlatoon.ShowInfoForAllWarriors();
+                _secondPlatoon.ShowInfoForAllWarriors();
+
+                RemoveAllDeadWarriors(_firstPlatoon);
+                RemoveAllDeadWarriors(_secondPlatoon);
+            }
+
+            Console.Clear();
+        }
+
+        public void Attack(Platoon attackingPlatoon, Platoon attackedPlatoon)
+        {
+            for (int i = 0; i < attackingPlatoon.GetCount(); i++)
+            {
+                Warrior attackingWarrior = attackingPlatoon.GetWarrior(i);
+
+                Warrior attackedWarrior = attackedPlatoon.GetWarrior(i);
+
+                if (attackedWarrior != null)
+                {
+                    if (attackedWarrior.IsAlive)
+                    {
+                        attackedWarrior.TakeDamage(attackingWarrior.Damage);
+                    }
+                }
+            }
+        }
+
+        public void RemoveAllDeadWarriors(Platoon platoon)
+        {
+            for (int i = platoon.GetCount() - 1; i >= 0; i--)
+            {
+                Warrior tempWarrior = platoon.GetWarrior(i);
+
+                if (tempWarrior.IsAlive == false)
+                {
+                    platoon.RemoveWarrior(tempWarrior);
+                }
+            }
+        }
+
+        public void ShowResultWar()
+        {
+            if (_firstPlatoon.GetCount() == 0 && _secondPlatoon.GetCount() == 0)
+            {
+                Console.WriteLine("Ничья!");
+            }
+            else if (_firstPlatoon.GetCount() > 0)
+            {
+                Console.WriteLine("Победила первая страна!");
+            }
+            else
+            {
+                Console.WriteLine("Победила вторая страна!");
+            }
+        }
+    }
+
+    class Platoon
+    {
+        private static Random _random = new Random();
+        private int _idWarrior;
+        private int _leftIndent;
+        private List<Warrior> _warriors = new List<Warrior>();
+
+        public Platoon(int leftIndent)
+        {
+            int _minCountWarriors = 25;
+            int _maxCountWarriors = 40;
+            int _countWarriors;
+            _idWarrior = 0;
+            _leftIndent = leftIndent;
+            _countWarriors = _random.Next(_minCountWarriors, _maxCountWarriors);
+
+            for (int i = 0; i < _countWarriors; i++)
+            {
+                _warriors.Add(new Warrior(++_idWarrior));
+            }
+        }
+
+        public void ShowInfoForAllWarriors()
+        {
+            int positionX = Console.CursorTop;
+
+            foreach (var warrior in _warriors)
+            {
+                warrior.ShowAllInfo(_leftIndent, positionX++);
+            }
+        }
+
+        public int GetCount()
+        {
+            return _warriors.Count;
+        }
+
+        public Warrior GetWarrior(int positionInList)
+        {
+            if (positionInList >= _warriors.Count)
+            {
+                return null;
+            }
+            else
+            {
+                return _warriors[positionInList];
+            }
+        }
+
+        public void RemoveWarrior(Warrior warrior)
+        {
+            _warriors.Remove(warrior);
+        }
+    }
+
+    class Warrior
+    {
+        private static Random _random = new Random();
+
+        public int ID { get; protected set; }
+
+        public int Health { get; protected set; }
+
+        public int Armor { get; protected set; }
+
+        public int Damage { get; protected set; }
+
+        public bool IsAlive { get; protected set; }
+
+        public Warrior(int id)
+        {
+            int minHealth = 70;
+            int maxHealth = 120;
+            int minDamage = 15;
+            int maxDamage = 25;
+            int minArmor = 5;
+            int maxArmor = 15;
+
+            ID = id;
+            IsAlive = true;
+            Health = _random.Next(minHealth, maxHealth);
+            Damage = _random.Next(minDamage, maxDamage);
+            Armor = _random.Next(minArmor, maxArmor);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            damage -= Armor;
+
+            if (damage > 0)
+            {
+                Health -= damage;
+
+                if (Health <= 0)
+                {
+                    Health = 0;
+                    IsAlive = false;
+                }
+            }
+        }
+
+        public void ShowAllInfo(int positionLeft, int positionTop)
+        {
+            Console.SetCursorPosition(positionLeft, positionTop);
+
+            if (IsAlive)
+            {
+                Console.Write("|ID:{0}\tHealth: {1}\t Armor: {2}\tDamage: {3}|", ID, Health, Armor, Damage);
+            }
+            else
+            {
+                Console.Write("|ID:{0}\t|-------------------DEAD------------------|", ID);
+            }
+
+            Console.SetCursorPosition(0, 0);
+        }
+    }
+}
